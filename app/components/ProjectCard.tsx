3baa4298033eb4662f2d6af5d63d2701ce51b8c2
Mandeep Singh <mandeepsingh.71ms31@gmail.com>
2024-08-03 01:46:12 -0400

    learnig Next.js part01

---
+++ app/components/ProjectCard.tsx
@@ -0,0 +1,13 @@
+'use client';
+import React from 'react'
+import AddToCart from './AddToCart';
+
+const ProjectCard = () => {
+  return (
+    <div>
+        <AddToCart/>
+    </div>
+  )
+}
+
+export default ProjectCard
